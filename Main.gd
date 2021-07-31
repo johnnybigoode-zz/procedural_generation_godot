578f78467b6d8b72df0ee93f0fe143e6e28c63b9
João A. Marson <jamarson@gmail.com>
2021-07-30 23:01:19 -0300

    Regenerate rooms with space, recarve tunnels with tab

--- Main.gd
+++ Main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var Room = preload("res://Room.tscn")
+onready var Map = $TileMap
 
 var tile_size = 32
 var num_rooms = 50
@@ -60,8 +61,9 @@ func _input(event):
 			n.queue_free()
 		path = null
 		make_rooms()
+	if event.is_action_pressed('ui_focus_next'):
+		make_map()
 	
-		
 func find_mst(nodes):
 	# Prim's algorithm
 	var path = AStar.new()
@@ -84,4 +86,67 @@ func find_mst(nodes):
 		path.connect_points(path.get_closest_point(current_position), free_id)
 		nodes.erase(min_point)
 	return path
+	
+func make_map():
+	#create TileMap from generated rooms and path
+	Map.clear()
+	
+	#fill tilemap with walls, then carve empty rooms
+	var full_rect = Rect2()
+	for room in $Rooms.get_children():
+		var r = Rect2(room.position - room.size, 
+			room.get_node("CollisionShape2D").shape.extents * 2)
+			
+		full_rect = full_rect.merge(r)
+	var topleft = Map.world_to_map(full_rect.position)
+	var bottomright = Map.world_to_map(full_rect.end)
+	for x in range(topleft.x, bottomright.x):
+		for y in range(topleft.y, bottomright.y):
+			Map.set_cell(x, y, 1)
+	
+	#carve rooms
+	var corridors = []
+	for room in $Rooms.get_children():
+		var s = (room.size / tile_size).floor()
+		var pos = Map.world_to_map(room.position)
+		var ul = (room.position / tile_size).floor() - s
+		for x in range(2, s.x * 2 - 1):
+			for y in range(2, s.y * 2 - 1):
+				Map.set_cell(ul.x + x, ul.y + y, 0)
 		
+		var p = path.get_closest_point(
+			Vector3(room.position.x, room.position.y,0)
+		)
+		
+		for conn in path.get_point_connections(p):
+			if not conn in corridors:
+				var start = Map.world_to_map(
+					Vector2(path.get_point_position(p).x,
+						path.get_point_position(p).y))
+				var end = Map.world_to_map(
+					Vector2(path.get_point_position(conn).x,
+						path.get_point_position(conn).y))
+				carve_path(start, end)
+			corridors.append(p)
+			
+func carve_path(pos1, pos2):
+	#carve a path between two points
+	var x_diff = sign(pos2.x - pos1.x)
+	var y_diff = sign(pos2.y - pos1.y)
+	
+	if x_diff == 0: x_diff = pow(-1.0, randi() % 2)
+	if y_diff == 0: y_diff = pow(-1.0, randi() % 2)
+	
+	var x_y = pos1
+	var y_x = pos2
+	if(randi() % 2) > 0:
+		x_y = pos2
+		y_x = pos1
+		
+	for x in range(pos1.x, pos2.x, x_diff):
+		Map.set_cell(x, x_y.y, 0)
+		Map.set_cell(x, x_y.y+y_diff, 0)
+	for y in range(pos1.y, pos2.y, y_diff):
+		Map.set_cell(y_x.x, y, 0)
+		Map.set_cell(y_x.x+x_diff, y, 0)
+	
