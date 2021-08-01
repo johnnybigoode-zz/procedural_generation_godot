710701bd3bdec947630938419e8cab4f098ff617
João A. Marson <jamarson@gmail.com>
2021-08-01 09:11:20 -0300

    Primitive debbuging, using the print() statement on Character class

--- Character.gd
+++ Character.gd
@@ -5,22 +5,34 @@ export var speed = 250
 
 var velocity = Vector2()
 
+#remove print statements
+
+func _ready():
+	print("Player spawned")
+
 func _input(event):
 	if event.is_action_pressed('scroll_up'):
 		$Camera2D.zoom = $Camera2D.zoom - Vector2(0.1, 0.1)
 	if event.is_action_pressed('scroll_down'):
 		$Camera2D.zoom = $Camera2D.zoom + Vector2(0.1, 0.1)
 
-func get_input():
+func print_pos():
+	print(self.position)
+
+func get_input():	
 	velocity = Vector2()
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
+		print_pos()
 	if Input.is_action_pressed('ui_left'):
 		velocity.x -= 1
+		print_pos()
 	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
+		print_pos()
 	if Input.is_action_pressed('ui_down'):
 		velocity.y += 1
+		print_pos()
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
